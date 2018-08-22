7a940d35c02f40f41315715d9c5ed6352b24721e
Renan Beux <nanerxueb@hotmail.com>
2018-08-22 15:14:00 -0300

    22/08/18 1513

---
+++ App10_Vagas/App10_Vagas/Banco/Database.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using App10_Vagas.Modelos;
+
+namespace App10_Vagas.Banco
+{
+    class Database
+    {
+        private SQLiteConnection _conexao;
+
+        public Database()
+        {
+            
+            _conexao = new SQLiteConnection();
+        }
+
+        public void Cadastro(Vaga vaga)
+        {
+
+        }
+
+        public List<Vaga> Consultar()
+        {
+            return null;
+        }
+
+        public Vaga ObterVagaPorId(int id)
+        {
+            return null;
+        }
+
+        public void Atualizacao(Vaga vaga)
+        {
+
+        }
+
+        public void Exclusao(int id)
+        {
+
+        }
+    }
+}
